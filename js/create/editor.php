6d9e3b9b0dfbaa1e5f03acec8df8c1d870111dab
fvmartin <fvmartin@users.noreply.github.com>
2022-10-24 13:56:41 +0200

    Revert "Enable CKEditor AutoSave Plugin for Sendy"
    This reverts commit cc2e11a1906bfc309f3f4f7ee3dc15ac67490fbf.

--- js/create/editor.php
+++ js/create/editor.php
@@ -6,13 +6,7 @@
 			allowedContent: true,
 			filebrowserUploadUrl: 'includes/create/upload.php?app=<?php echo get_app_info('app');?>',
 			height: '570px',
-			extraPlugins: 'codemirror,dragresize,autosave'
-			,autosave: {
-					// discard autosave on Save, Cancel or when switching to Sendy HTML editor
-					saveDetectionSelectors: "a[href^='javascript:__doPostBack'][id*='Save'],a[id*='Cancel'],#toggle-wysiwyg"
-					, delay: 300
-					,'messageType': 'statusbar'
-			}
+			extraPlugins: 'codemirror,dragresize'
 			<?php if($dark_mode):?>
 			,skin: 'moono-dark'
 			<?php else:?>
@@ -22,16 +16,16 @@
 		
 		// wait until the editor is done initializing
 		ckEditor.on("instanceReady",function() {
-			// overwrite the default save function
-			ckEditor.addCommand( "save", {
-				modes : { wysiwyg:1, source:1 },
-				exec : function () {
-					// get the editor content
-					var theData = ckEditor.getData();
-					$('<input>').attr({type: 'hidden',id: 'save-only',name: 'save-only',value: 1}).appendTo('form');
-					$("#campaign-save-only-btn, #autoresponder-save-only-btn, #save-button").click();
-				}
-			});
+		  // overwrite the default save function
+		  ckEditor.addCommand( "save", {
+		    modes : { wysiwyg:1, source:1 },
+		    exec : function () {
+		      // get the editor content
+		      var theData = ckEditor.getData();
+		      $('<input>').attr({type: 'hidden',id: 'save-only',name: 'save-only',value: 1}).appendTo('form');
+		      $("#campaign-save-only-btn, #autoresponder-save-only-btn, #save-button").click();
+		    }
+		   });
 		});
 		
 		//Save campaign only
